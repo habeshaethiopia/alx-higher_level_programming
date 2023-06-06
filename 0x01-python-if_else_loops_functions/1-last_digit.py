995d9b8074b9ddd07edeeccd401f136598c5c795
Adane Moges <adanemogs6@gmail.com>
2023-06-06 20:32:07 +0300

    adding is

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ else:
     last = abs(number) % 10 * -1
 print("Last digit of {} is {} ".format(number, last), end="")
 if last > 5:
-    print("and greater than 5")
+    print("and is greater than 5")
 elif last == 0:
     print("and equal to 0")
 elif last < 6:
-    print("and less than 6")
+    print("and is less than 6")
