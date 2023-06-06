d157b1d8472055064b11baf9deaf6c5f963e3c7a
Adane Moges <adanemogs6@gmail.com>
2023-06-06 20:34:20 +0300

    simple copy past problem fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ print("Last digit of {} is {} ".format(number, last), end="")
 if last > 5:
     print("and is greater than 5")
 elif last == 0:
-    print("and equal to 0")
+    print("and is 0")
 elif last < 6:
-    print("and is less than 6")
+    print("and is less than 6 and not 0")
