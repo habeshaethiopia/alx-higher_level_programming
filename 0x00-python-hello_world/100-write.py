669e6f210be68865e5899dfbcba2d55ccdf05f43
Adane Moges <adanemogs6@gmail.com>
2023-06-08 01:53:32 +0300

    last

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import sys
-for a in "and that piece of art is useful - Dora Korpar, 2015-10-19\n":
-    sys.stdout.write(a)
-sys.stdout.flush()
+sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
+sys.exit()
