399cb2486e259c229ec145008ffab119dbffdee9
Adane Moges <adanemogs6@gmail.com>
2023-06-09 00:46:02 +0300

    updating

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-if __name__ == __main__:
+if __name__ == '__main__':
     print("{} + {} = {}".format(10, 5, add(a, b)))
     print("{} - {} = {}".format(10, 5, sub(a, b)))
     print("{} * {} = {}".format(10, 5, mul(a, b)))
