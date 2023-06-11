eabba06f9e3721137ff3fcd3c6397cff28264b1f
Adane Moges <adanemogs6@gmail.com>
2023-06-11 22:57:14 +0300

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if (idx < 0 or idx >= len(my_list)):
+        return (my_list[:])
+    t = my_list[:]
+    t[idx] = element
+    return (t)
