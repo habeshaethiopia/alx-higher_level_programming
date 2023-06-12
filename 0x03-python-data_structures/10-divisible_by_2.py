a57257495d4bf0801de14bd7da4dfd5c85e7dffc
Adane Moges <adanemogs6@gmail.com>
2023-06-12 20:47:31 +0300

    task 10 done

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    divB2 = []
+    if len(my_list) == 0:
+        return None
+    for i in my_list:
+        if i % 2 == 0:
+            divB2.append(True)
+        else:
+            divB2.append(False)
+    return divB2
