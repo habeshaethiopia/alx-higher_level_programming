8c0f7a12e0b545c69743ac54df8ce379645e0207
Adane Moges <adanemogs6@gmail.com>
2023-06-12 08:10:50 +0300

    task 6 done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    d = " "
+    if matrix:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                print(matrix[i][j], end=d)
+                if j == len(matrix[i])-2:
+                    d = "\n"
+    else:
+        print()
