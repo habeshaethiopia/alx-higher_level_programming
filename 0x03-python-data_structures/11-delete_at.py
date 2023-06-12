fcd867bf1cefde7589347f0f5a8247128339be2d
Adane Moges <adanemogs6@gmail.com>
2023-06-12 21:42:29 +0300

    task 11 done

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if len(my_list) == 0:
         return None
-    if idx > len(my_list)-1 or idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return my_list
     my_list.remove(my_list[idx])
     return my_list
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,12 @@
+#include"lists.h"
+/**
+ * is_palindrome - afunction to check if the linked list is palindrom or not
+ *
+ * @head: the head of the list
+ * Return: int 0 or 1
+ *
+ */
+int is_palindrome(listint_t **head)
+{
+
+}
