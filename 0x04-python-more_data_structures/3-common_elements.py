0e640c6a581cb5f8d7297cf83675574607f76349
Adane Moges <adanemogs6@gmail.com>
2023-06-15 06:01:36 +0300

    task3 done

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    re = []
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                re.append[i]
+    return re
