5a0a1914a6b3d2a4334766540ae16bbb80f065d0
Adane Moges <adanemogs6@gmail.com>
2023-06-15 05:57:19 +0300

    task 2 is done

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = 0
+    list = []
+    for i in my_list:
+        f = 0
+        for j in my_list:
+            if i == j:
+                f += 1
+        if f == 1:
+            sum += i
+    return sum
