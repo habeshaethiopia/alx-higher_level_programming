5ad54ad7c6b9492c9b7abfa32e7fee49f1d36054
Adane Moges <adanemogs6@gmail.com>
2023-06-15 06:33:08 +0300

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    List = my_list[:]
+    for i in range(len(List)):
+        if List[i] == search:
+            List[i] = replace
+    return List
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,7 +2,6 @@
 def common_elements(set_1, set_2):
     re = []
     for i in set_1:
-        for j in set_2:
-            if i == j:
-                re.append[i]
+        if i in set_2:
+            re.append[i]
     return re
