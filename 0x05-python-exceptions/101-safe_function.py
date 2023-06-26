f2cf46d6d14eef56b7f091c60df8504ab9a1200c
Adane Moges <adanemogs6@gmail.com>
2023-06-27 00:43:05 +0300

    task 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        x = fct(*args)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
+    return x
