4903aae2f6c7863eb150ce7049e5176eb24b2afc
Adane Moges <adanemogs6@gmail.com>
2023-06-26 23:06:03 +0300

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except IndexError:
+            raise
+        except Exception:
+            pass
+        else:
+            n += 1
+    print()
+    return n
