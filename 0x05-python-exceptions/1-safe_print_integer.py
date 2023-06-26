be09b4dfe75b28ecd3e633234786a869cf4df376
Adane Moges <adanemogs6@gmail.com>
2023-06-26 20:47:08 +0300

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{}".format(value))
+        if notisdigit(value):
+            raise Exception
+        return True
+    except Exception:
+        return False
