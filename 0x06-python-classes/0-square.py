3d5e17c8789c49d07d32f6ef0d9e8397e1971109
Adane Moges <adanemogs6@gmail.com>
2023-06-28 06:14:27 +0300

    first

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This module contains a square class"""
 class Square:
     """Empty square class."""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This module contains a square class"""
 class square():
     """A square class """
 
