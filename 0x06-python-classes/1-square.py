449808691f5159eea034e4273ed7b5e9f0fdf02e
Adane Moges <adanemogs6@gmail.com>
2023-06-28 21:54:44 +0300

    add documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""This module contains a square class"""
+"""This module contains a square class
+and contain a constracter that accept a variable 
+size and assign to the private attribute of the class
+"""
 
 
 class square():
