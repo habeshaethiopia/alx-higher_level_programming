0200bc04ae6aa2137e77a3bc843b3465d17e6f28
Adane Moges <adanemogs6@gmail.com>
2023-06-28 06:00:00 +0300

    last

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,10 +2,13 @@
 class square():
     """A square class """
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Initialization of the class"""
         try:
-            if is
+            if type(size) != int:
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
             self.__size = size
         except TypeError as t:
             print(t)
