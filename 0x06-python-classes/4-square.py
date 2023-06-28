b94e079039a1b9737cc9089240a4ecac9ca55aad
Adane Moges <adanemogs6@gmail.com>
2023-06-29 02:00:05 +0300

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""This modeule contain a square class"""
+
+
+class Square:
+    """A square class """
+
+    def __init__(self, size=0):
+        """Initialization of the class"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ a function which returns the Area"""
+        return self.__size*self.__size
+
+    def size(self):
+        """ Function that returns the size of the square"""
+        return self.__size
+
+    def size(self, value):
+        """setter fir size"""
+        self.__size = size
