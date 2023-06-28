5cc21cb0fadcf36fdc2313192120ea87fde11b2f
Adane Moges <adanemogs6@gmail.com>
2023-06-29 02:36:48 +0300

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,57 @@
 #!/usr/bin/python3
+"""This modeule contain a square class"""
+
+
+class Square:
+    """A square class """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialization of the class"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        if type(position) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def area(self):
+        """ a function which returns the Area"""
+        return self.__size*self.__size
+
+    @property
+    def size(self):
+        """ Function that returns the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """ function to print asquare compoas of "#" symbol"""
+        if (self.__size == 0):
+            print()
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+            print()
+
+    @property
+    def position(self):
+        """ Function that returns the position of the square"""
+        return self.__position
+
+    @position.setter
+    def position(self, position=(0, 0)):
+        """positon setter:"""
+        if type(position) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
