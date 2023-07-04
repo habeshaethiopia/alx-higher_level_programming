b13a92252e4a85e87a8e97f35e6497352c1f76f7
Adane Moges <adanemogs6@gmail.com>
2023-07-04 15:19:35 +0300

    101advance

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def magic_string():
-    setattr(magic_string, 'c', getattr(magic_string, 'c', -1)+1)
-    return "BestSchool"+", BestSchool"*magic_string.c
+
+
+class LockedClass:
+    __slots__ = ["first_name"]
