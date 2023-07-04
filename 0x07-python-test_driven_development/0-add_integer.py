1f02fdc0187a02abe1ec7bc0b34431fe951d5c2a
Adane Moges <adanemogs6@gmail.com>
2023-07-04 18:04:58 +0300

    task 0 and 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""A module with afnction to add two integers"""
+
+
 def add_integer(a, b=98):
-    
+    """ this function is to add 
+    two integers and handling all the
+    posible test cases
+    """
     if type(a) != int:
         raise TypeError("a must be an integer")
     elif type(a) == float:
-        a= int(a)
+        a = int(a)
     if type(b) != int:
         raise TypeError("a must be an integer")
     elif type(b) == float:
-        b= int(b)
+        b = int(b)
     return a+b
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+""" module cantaining matrix_divided(matrix, div) function"""
+
+
+def matrix_divided(matrix, div):
+    """afunction which divide each element of a matrix"""
+    Emess = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) != list or len(matrix) == 0 or matrix[0] is None:
+        raise TypeError(Emess)
+    for r in matrix:
+        if len(r) == 0:
+            raise TypeError(Emess)
+        for i in r:
+            if type(i) != int and type(i) != float:
+                raise TypeError(Emess)
+
+    l = len(matrix[0])
+    for r in matrix:
+        if l == len(r):
+            raise TypeError("Each row of the matrix must have the same size")
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_m = []
+    for r in matrix:
+        row = []
+        for v in r:
+            result = round(v/div, 2)
+            row.append(result)
+        new_m.append(row)
+    return new_m
