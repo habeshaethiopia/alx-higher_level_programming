4dee8f6e4a2bd54bb79ff29605b943344b16a530
Adane Moges <adanemogs6@gmail.com>
2023-07-05 17:10:45 +0300

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+
+"""
+This is a module for a class Rectangle
+"""
+
+
+class Rectangle:
+    """Class of a Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize class"""
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """to get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """to set width"""
+        self.__width = value
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """Get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height"""
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
