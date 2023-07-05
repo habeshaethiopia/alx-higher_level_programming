4a066299680f26bb37e1684879cde0d6ebf7fd1e
Adane Moges <adanemogs6@gmail.com>
2023-07-05 05:43:27 +0300

    3 and 4

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" THE module that prints ma name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ A method to say my name"""
+    if type(first_name) != str or type(last_name) != str:
+        raise TypeError(
+            "first_name must be a string or last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" A module cointaining print_square function"""
+
+
+def print_square(size):
+    """ a function that prints a square with the character '#'"""
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
