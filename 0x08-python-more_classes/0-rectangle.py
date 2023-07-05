9ae00536e05543aa5d6286c544d6806e282bd1fb
Adane Moges <adanemogs6@gmail.com>
2023-07-05 16:57:16 +0300

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""The module contain rectangle class """
+"""
+The module contain rectangle class.
+"""
 
 
 class rectangle:
-    """this is class documentation """
+    """this is class documentation."""
 
     def __init__(self):
-        """class constrCTOR"""
+        """class constrCTOR."""
         pass
