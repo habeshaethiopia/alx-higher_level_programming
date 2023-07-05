ce5664d83ba5d67b21dd8f3d63ac669da2c0b7ac
Adane Moges <adanemogs6@gmail.com>
2023-07-05 05:53:10 +0300

    the last one

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Amodule containing a function called text_indentation """
+
+
+def text_indentation(text):
+    """  function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    delimiter = [".", "?", ":"]
+    for c in text:
+        if c in delimiter:
+            print()
+            print()
+        print(c, end="")
