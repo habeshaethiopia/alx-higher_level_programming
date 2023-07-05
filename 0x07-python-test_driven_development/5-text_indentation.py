a2094aecf5a5b0ffd722fedfe6f7d22d455533fe
Adane Moges <adanemogs6@gmail.com>
2023-07-05 06:06:06 +0300

    5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,8 +7,14 @@ def text_indentation(text):
     if type(text) != str:
         raise TypeError("text must be a string")
     delimiter = [".", "?", ":"]
+    new_line = 0
     for c in text:
         if c in delimiter:
-            print()
-            print()
-        print(c, end="")
+            print(c, end="\n\n")
+            new_line = 1
+        else:
+            if new_line == 1 and (c == " " or c == "\t"):
+                pass
+            else:
+                print(c, end="")
+                new_line = 0
