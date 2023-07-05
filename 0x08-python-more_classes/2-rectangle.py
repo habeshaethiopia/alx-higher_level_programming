becac908abf473a0e09754c2ad04b19e8eaf4c80
Adane Moges <adanemogs6@gmail.com>
2023-07-05 17:35:09 +0300

    task 2 correcting return zero

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,6 +44,6 @@ class Rectangle:
 
     def perimeter(self):
         """calculate perimeter"""
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
         return 2*(self.__width+self.__height)
