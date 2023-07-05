fce8397217bf99dc9e261c4ed02ededc68d4f3ab
Adane Moges <adanemogs6@gmail.com>
2023-07-05 17:12:28 +0300

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,9 +22,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """to set width"""
-        self.__width = value
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
