0d05a30ba0cfcf4c707958a01d40edf168d9e36d
Adane Moges <adanemogs6@gmail.com>
2023-07-10 18:58:59 +0300

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 """the modile document"""
+Rectangle = __import__('9-rectangle').Rectangl
+
+
+class Square(Rectangle):
+    """class squre"""
+
+    def __init__(self, size):
+        """constractor"""
+        self.integer_validator("size", size)
+        self.size = size
+
+    def area(self):
+        """the area"""
+        return self.size**2
