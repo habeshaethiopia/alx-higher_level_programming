883014b64fe9a2888e2247a1162ce9420af36ca1
Adane Moges <adanemogs6@gmail.com>
2023-07-10 18:42:57 +0300

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
 """the modile document"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """aclass inherit from base Geometry"""
+
+    def __init__(self, width, height):
+        """The constractor"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self._width = width
+        self._height = height
+
+    def area(self):
+        """impliment the area"""
+        return self._height * self._width
+
+    def __str__(self):
+        """the string"""
+        print("[Rectangle] {}/{}".format(self._width, self._height))
