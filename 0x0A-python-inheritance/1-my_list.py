92e3962a067b5e709271c5e520f6bb79db8b179c
Adane Moges <adanemogs6@gmail.com>
2023-07-10 17:11:37 +0300

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Module contain MyList class"""
+
+
+class MyList(list):
+    """the class"""
+
+    def print_sorted(self):
+        """To sort the list"""
+        print(sorted(self))
