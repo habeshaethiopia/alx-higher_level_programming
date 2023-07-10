ebb94e7daf7dfbece40eb1d412cce7fc03106a3a
Adane Moges <adanemogs6@gmail.com>
2023-07-10 17:54:51 +0300

    task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 """the modile document"""
+
+
+class BaseGeometry:
+    """a New class"""
+
+    def area(self):
+        """the area"""
+        raise Exception("area() is not implemented")
