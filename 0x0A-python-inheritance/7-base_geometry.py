4875830a2d1887ac40fa28ede7ba2c2a68354ebc
Adane Moges <adanemogs6@gmail.com>
2023-07-10 18:03:48 +0300

    task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
 """the modile document"""
+
+
+class BaseGeometry:
+    """a New class"""
+
+    def area(self):
+        """the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Intiger Validater"""
+        if type(value) == int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
