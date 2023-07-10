5f81584f43b7880c3502a9e4c8d38f519dfb283f
Adane Moges <adanemogs6@gmail.com>
2023-07-10 18:35:39 +0300

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,12 +2,13 @@
 """the modile document"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """aclass inherit from base Geometry"""
 
     def __init__(self, width, height):
         """The constractor"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self._width = width
         self._height = height
-        super.integer_validator("width", width)
-        super.integer_validator("height", height)
