3efb0cd534b7f234dc324ea3ffd5472509440b90
Adane Moges <adanemogs6@gmail.com>
2023-07-11 05:29:47 +0300

    advanced task

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """module document"""
+
+
 def add_attribute(object, attribute, value):
     """adds a new attribute"""
-    
-    setattr(object,attribute,value)
+    if not hasattr(object, "__slots__") and not hasattr(object, "__dict__"):
+        raise TypeError("can't add new attribute")
+    if hasattr(object, "__slots__") and not hasattr(object, attribute):
+        raise TypeError("can't add new attribute")
+    setattr(object, attribute, value)
