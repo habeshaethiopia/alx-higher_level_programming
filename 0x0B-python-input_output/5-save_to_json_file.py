b039cda6bd63007094a43d43655c7b7ee801ac7a
Adane Moges <adanemogs6@gmail.com>
2023-07-12 04:36:37 +0300

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,2 +1,9 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 """the module doc"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """the file .json"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
