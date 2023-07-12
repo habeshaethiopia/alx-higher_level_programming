88c8f3fff00f6652c11d4269d0b1960cd1116afb
Adane Moges <adanemogs6@gmail.com>
2023-07-12 04:43:13 +0300

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,2 +1,9 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 """the module doc"""
+import json
+
+
+def load_from_json_file(filename):
+    """json read"""
+    with open(filename) as f:
+        return json.load(f)
