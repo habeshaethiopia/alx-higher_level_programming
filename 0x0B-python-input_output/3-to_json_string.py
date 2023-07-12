ea3e50fe5056596c1872508221fc1ce21b30cc0a
Adane Moges <adanemogs6@gmail.com>
2023-07-12 04:26:31 +0300

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """the module doc"""
 
+import json
+
 
 def to_json_string(my_obj):
-    """function doc"""
-    import json
+    """function return JSON representstion"""
     return json.dumps(str(my_obj))
