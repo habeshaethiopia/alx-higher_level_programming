7f9339c6283ea84b6b95ff76d2480a824741cc75
Adane Moges <adanemogs6@gmail.com>
2023-07-12 05:22:39 +0300

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,2 +1,16 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 """the module doc"""
+
+
+class Student:
+    """The class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """The constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """the dict of the obj"""
+        return self.__dict__
