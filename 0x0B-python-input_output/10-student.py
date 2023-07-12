f1cf8548cea1d037d0badadf7a7c54c3649e836f
Adane Moges <adanemogs6@gmail.com>
2023-07-12 05:34:12 +0300

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,2 +1,23 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 """the module doc"""
+
+
+class Student:
+    """The class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """The constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """the dict of the obj"""
+        retu = []
+        if all(type(item) == str for item in attrs):
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    retu.append({key: value})
+            return retu
+        else:
+            return self.__dict__
