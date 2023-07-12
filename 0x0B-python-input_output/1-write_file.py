764fc81897dda218a86e8819175bbde25b79c501
Adane Moges <adanemogs6@gmail.com>
2023-07-12 04:18:25 +0300

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
+"""the module doc"""
+
+
+def write_file(filename="", text=""):
+    """function doc"""
+    with open(filename, "w") as f:
+        return f.write(text)
