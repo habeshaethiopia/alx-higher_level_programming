e599ed0e1bfb95d6c21180bcbef68d5378942b2f
Adane Moges <adanemogs6@gmail.com>
2023-07-12 05:10:15 +0300

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,2 +1,18 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 """the module doc"""
+import sys
+import os.path
+arg = sys.argv[1:]
+save_j = __import__('5-save_to_json_file').save_to_json_file
+load_j = __import__('6-load_from_json_file').load_from_json_file
+
+
+def main():
+    """the main function"""
+    str = []
+    if os.path.exists('add_item.json'):
+        str = load_j('add_item.json')
+    save_j(str + arg, "add_item.json")
+
+
+main()
