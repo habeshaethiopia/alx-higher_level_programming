f4df74f16839030d9f0e1cce86b6aa4570b2aa32
Adane Moges <adanemogs6@gmail.com>
2023-07-12 06:00:01 +0300

    uidhiu

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -17,12 +17,3 @@ def pascal_triangle(n):
                     row.append(lis[i-1][j-1]+lis[i-1][j])
             lis.append(row)
         return lis
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-print_triangle(pascal_triangle(5))
