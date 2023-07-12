0c218992c7311c3c68c0ca432880a5186185f7be
Adane Moges <adanemogs6@gmail.com>
2023-07-12 05:59:39 +0300

    uidhiu

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,2 +1,28 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 """the module doc"""
+
+
+def pascal_triangle(n):
+    """the Technical interview preparation"""
+    lis = []
+    if n <= 0:
+        return
+    else:
+        for i in range[n]:
+            row = []
+            for j in range[i]:
+                if j == i[(len[i] - 1)] or j == 0:
+                    row.append(1)
+                else:
+                    row.append(lis[i-1][j-1]+lis[i-1][j])
+            lis.append(row)
+        return lis
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+print_triangle(pascal_triangle(5))
