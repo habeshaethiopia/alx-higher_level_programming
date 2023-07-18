c7ab53ad28817d306c09f4fb3781175462684b2a
Adane Moges <adanemogs6@gmail.com>
2023-07-18 20:20:19 +0300

    task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,8 +17,22 @@ class Base:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """to json"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
+        if type(list_dictionaries) != list:
+            raise TypeError("list_dictionaries must be a list")
+        if any(type(i) != dict for i in list_dictionaries):
+            raise TypeError("list_dictionaries must contain dictionaries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to the file"""
+        if not isinstance(cls, list_objs):
+            raise TypeError("list_objs must be sub class of Base")
+        with open(str(cls)+".json", "w") as f:
+            res = to_json_string(list_objs)
+            f.write(res)
