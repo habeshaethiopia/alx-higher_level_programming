56aefa46ef79436a1e8dcffa59e58d2b043866c6
Adane Moges <adanemogs6@gmail.com>
2023-07-18 14:16:29 +0300

    squre class created

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module doc"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """squre class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """The constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """the string Notation"""
+        return "[Square] ("+str(self.id) + ") "+str(self.__x) +\
+            "/"+str(self.__y)+" - "+str(self.__width)
