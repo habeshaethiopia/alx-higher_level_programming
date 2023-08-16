87e1be886b7818a0c6c2ebdff9363496fef6aef4
Adane Moges <adanemogs6@gmail.com>
2023-08-16 04:11:10 +0300

    the last commit message for today

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -2,4 +2,5 @@
 -- Write a script that displays the max temperature of each state (ordered by State name).
 SELECT state, MAX(value) as max_temp
 from temperatures  
-GROUP BY (state);
+GROUP BY (state)
+ORDER BY state;
