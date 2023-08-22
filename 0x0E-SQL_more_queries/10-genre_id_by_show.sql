8c308a8ffdebfed8fbb831f41ae58f3a82c3fc3e
Adane Moges <adanemoges6@gmail.com>
2023-08-22 18:25:55 +0300

    task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- mport the database dump from hbtn_0d_tvshows to your MySQL server: download
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SOURCE hbtn_0d_tvshows.sql;
+
 SELECT tv_shows.title ,tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
