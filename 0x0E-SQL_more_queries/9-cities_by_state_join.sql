7f79f9e35e2a54b42bb1940eea3c761fe394d2b1
Adane Moges <adanemoges6@gmail.com>
2023-08-22 16:51:37 +0300

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,5 @@
+-- Write a script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
 SELECT cities.id, cities.name, states.name
 FROM cities JOIN states
 ON  states.id=cities.state_id
