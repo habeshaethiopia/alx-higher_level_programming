f7f32e1e543ad6fdf1dfdf5b01baa159266f1a94
Adane Moges <adanemoges6@gmail.com>
2023-08-22 14:18:06 +0300

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,13 +5,11 @@
 -- name VARCHAR(256) can’t be null
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
-CREATE DATABASE
-IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE
-IF NOT EXISTS states
-{
+CREATE TABLE IF NOT EXISTS states
+(
     id INT UNIQUE,
     name VARCHAR
 (256) NOT NULL
-}
+);
