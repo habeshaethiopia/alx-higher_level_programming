37c0a0081f4639482cd4c4d839b346ab5909b1b7
Aadane <adanemoges6@gmail.com>
2023-09-13 02:05:05 +0300

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurences = function (list, searchElement) {
   let n = 0;
   let se;
   for (se in list) {
-    if (se === searchElement) {
+    if (list[se] === searchElement) {
       n++;
     }
   }
