ed630849498d1e2a712af5035b863b2caa64d372
Aadane <adanemoges6@gmail.com>
2023-09-13 01:00:37 +0300

    task 3

--- 0x13-javascript_objects_scopes_closures/2-main.js
+++ 0x13-javascript_objects_scopes_closures/2-main.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const Rectangle = require('./2-rectangle');
+const Rectangle = require('./3-rectangle');
 
 const r1 = new Rectangle(2, 3);
 console.log(r1);
 console.log(r1.width);
 console.log(r1.height);
+r1.print();
 
 const r2 = new Rectangle(2, -3);
 console.log(r2);
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,8 +10,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) { console.log('X'); }
-      console.log('\n');
+      console.log('X'.repeat(this.width));
     }
   }
 }
