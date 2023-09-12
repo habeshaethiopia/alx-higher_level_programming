4c7d603722aea74a6de0b743f0645a4ada1d9f26
Aadane <adanemoges6@gmail.com>
2023-09-13 01:30:49 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    for (const i in Range(this.height)) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+module.exports = Square;
