8909d385146b3a1692c2d8512fc389efc77791c9
Aadane <adanemoges6@gmail.com>
2023-09-12 20:14:14 +0300

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv;
+const le = args.length;
+if (le === 1) {
+  console.log('no argument');
+} else if (le === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
