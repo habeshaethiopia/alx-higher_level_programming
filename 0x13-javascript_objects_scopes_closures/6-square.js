20d6972ec832d9778edb2f1f1468545bb9f4b966
Aadane <adanemoges6@gmail.com>
2023-09-13 01:46:59 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,7 +6,10 @@ class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint (c = 'X') {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.height));
     }
