6ed6bb5ac6497db67378a078de7220a7b84c5f4a
Aadane <adanemoges6@gmail.com>
2023-09-12 20:45:18 +0300

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const args = process.argv;
+const conv = parseInt(args[2]);
+if (conv && args[2]) {
+  console.log(`My number: ${conv}`);
+} else {
+  console.log('Not a number');
+}
