7b829c8f03a98ea97483c17c2c7e627f11a03a5e
Aadane <adanemoges6@gmail.com>
2023-09-13 02:14:20 +0300

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const rlist = [];
+  for (let i = list.length() - 1, j = 0; i >= 0; i--, j++) {
+    rlist[j] = list[i];
+  }
+  return rlist;
+};
