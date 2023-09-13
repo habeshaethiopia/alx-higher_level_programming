8aa3de97afca7df4c756b2afde3fc259c8bc8d9e
Aadane <adanemoges6@gmail.com>
2023-09-13 20:51:33 +0300

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const arg = process.argv;
+function factorial (a = 0) {
+  if (!a) { return 1; } else { return a * factorial(a - 1); }
+}
+const a = parseInt(arg[2]);
+console.log(factorial(a));
