37dfa7cc6dc22bbce2680d2011543e2849b8df83
Aadane <adanemoges6@gmail.com>
2023-09-13 20:36:28 +0300

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+const args = process.argv;
+if (!args[2] || !parseInt(args[2])) { console.log('Missing number of occurrences'); } else {
+  for (let i = 0; i < parseInt(args[2]); i++) { console.log('C is fun'); }
+}
