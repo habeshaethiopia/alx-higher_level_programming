c935e45449056bc167c6d5c8460d8aafdbe75698
Aadane <adanemoges6@gmail.com>
2023-09-13 03:41:10 +0300

    advace task task 1

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const map1 = list.map((x) => {
-  return list.indexOf(x) * x;
+const map1 = list.map((x, index) => {
+  return index * parseInt(x);
 });
 console.log(list);
 console.log(map1);
