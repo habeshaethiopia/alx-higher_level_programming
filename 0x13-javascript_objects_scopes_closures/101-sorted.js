7361fc0f163c5db04cf5cb4aedc666bb51cd5e2d
Aadane <adanemoges6@gmail.com>
2023-09-13 04:20:10 +0300

    advace task task 2

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const dict = require("./101-data.js").dict;
+const dic = {};
+for (const key in dict) {
+  if (dict[key] in dic) {
+    dic[dict[key]].push(key);
+  } else {
+    dic[dict[key]] = [key];
+  }
+}
+
+console.log(dic);
