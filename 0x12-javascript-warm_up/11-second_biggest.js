f9a3daa510ab2949770915a64d6506ee06489c4d
Aadane <adanemoges6@gmail.com>
2023-09-13 21:02:19 +0300

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const arg = process.argv;
+if (!arg[3]) console.log(0);
+else {
+  const newarr = [];
+  for (let i = 2; i < arg.length; i++) {
+    newarr.push(parseInt(arg[i]));
+  }
+  newarr.sort();
+  console.log(newarr[newarr.length - 2]);
+}
