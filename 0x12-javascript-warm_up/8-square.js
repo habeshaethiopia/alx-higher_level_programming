c0128a026045a0ab1573b4bef7c6bc020661da56
Aadane <adanemoges6@gmail.com>
2023-09-13 20:40:40 +0300

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv;
+if (!args[2] || !parseInt(args[2])) {
+  console.log('Missing size');
+} else {
+  const size = parseInt(args[2]);
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
