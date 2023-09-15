8ea1745f8f27c8176ee5c05d93fc696d8c3f26cf
Aadane <adanemoges6@gmail.com>
2023-09-15 16:27:15 +0300

    first git

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,19 +3,19 @@
 what is inside the states table"""
 from sys import argv
 import MySQLdb
+if __name__ == '__main__':
+    MY_USER = argv[1]
+    MY_PASS = argv[2]
+    MY_DB = argv[3]
 
-MY_USER = argv[1]
-MY_PASS = argv[2]
-MY_DB = argv[3]
+    db = MySQLdb.connect(host="localhost", user=MY_USER, passwd=MY_PASS, db=MY_DB)
 
-db = MySQLdb.connect(host="localhost", user=MY_USER, passwd=MY_PASS, db=MY_DB)
-
-cur = db.cursor()
-cur.execute("select * from states order by states.id asc;")
-rows = cur.fetchall()
-tup = ()
-for row in rows:
-    for col in row:
-        tup = tup + (col,)
-    print(tup)
+    cur = db.cursor()
+    cur.execute("select * from states order by states.id asc;")
+    rows = cur.fetchall()
     tup = ()
+    for row in rows:
+        for col in row:
+            tup = tup + (col,)
+        print(tup)
+        tup = ()
