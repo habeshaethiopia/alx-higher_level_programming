a7ed1ac7a0a57712043f9227ade804fcfbf4779b
Aadane <adanemoges6@gmail.com>
2023-09-15 16:23:35 +0300

    first git

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this module is for connecting to the mysql database and to print
+what is inside the states table"""
+from sys import argv
+import MySQLdb
+
+MY_USER = argv[1]
+MY_PASS = argv[2]
+MY_DB = argv[3]
+
+db = MySQLdb.connect(host="localhost", user=MY_USER, passwd=MY_PASS, db=MY_DB)
+
+cur = db.cursor()
+cur.execute("select * from states order by states.id asc;")
+rows = cur.fetchall()
+tup = ()
+for row in rows:
+    for col in row:
+        tup = tup + (col,)
+    print(tup)
+    tup = ()
