504394b9b5815891de29893e4ec86c68b403b28a
Aadane <adanemoges6@gmail.com>
2023-09-15 16:37:32 +0300

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""this module is for connecting to the mysql database and to print
+what is inside the states table that start with cpaital 'N'"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    MY_USER = argv[1]
+    MY_PASS = argv[2]
+    MY_DB = argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         user=MY_USER, passwd=MY_PASS, db=MY_DB)
+
+    cur = db.cursor()
+    cur.execute(
+        "select * from states where name like 'N%' \
+                order by states.id asc;"
+    )
+    rows = cur.fetchall()
+    tup = ()
+    for row in rows:
+        for col in row:
+            tup = tup + (col,)
+        print(tup)
+        tup = ()
