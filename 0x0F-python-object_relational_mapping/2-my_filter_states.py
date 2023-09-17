6834a5e1e40b146c603c1778828dd9ef6cdad533
Aadane <adanemoges6@gmail.com>
2023-09-18 01:05:11 +0300

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""this module is for connecting to the mysql database and to print
+what is inside the states table that where 'name' matches the argument."""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    MY_USER = argv[1]
+    MY_PASS = argv[2]
+    MY_DB = argv[3]
+    MY_AEG = argv[4]
+    Query = "select * from states where \
+        name = \"{}\" \
+                order by states.id asc;".format(MY_AEG)
+    db = MySQLdb.connect(host="localhost",
+                         user=MY_USER, passwd=MY_PASS, db=MY_DB)
+
+    cur = db.cursor()
+    cur.execute(Query)
+    rows = cur.fetchall()case
+    tup = ()
+    for row in rows:
+        for col in row:
+            tup = tup + (col,)
+        print(tup)
+        tup = ()
