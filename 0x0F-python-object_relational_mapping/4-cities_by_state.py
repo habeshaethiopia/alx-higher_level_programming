71523f3e1d265060f79b2c0656cfdb8f199ec153
Aadane <adanemoges6@gmail.com>
2023-09-18 02:03:46 +0300

    Merge branch 'master'

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     MY_USER = argv[1]
     MY_PASS = argv[2]
     MY_DB = argv[3]
-    Query = "select * from cities order by cities.id asc;"
+    Query = "select cities.id,cities.name as cities,states.name from cities\
+                    inner join(states) on states.id = cities.state_id\
+                    order by cities.id asc;"
     db = MySQLdb.connect(host="localhost",
                          user=MY_USER, passwd=MY_PASS, db=MY_DB)
 
