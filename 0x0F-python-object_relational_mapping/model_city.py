129d4f113fb5b3724b495915d42ef51a79dfb014
Aadane <adanemoges6@gmail.com>
2023-09-18 05:59:16 +0300

    model

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""the orm implimentationl using sqlalchemy"""
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+
+class City(Base):
+    """for state class"""
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128))
+    state_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    __tablename__ = "states"
+    __tablename__
