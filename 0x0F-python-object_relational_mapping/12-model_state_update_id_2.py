3853f1b990223dea06916d25cb13d7b07b140cae
Aadane <adanemoges6@gmail.com>
2023-09-18 05:53:41 +0300

    task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""using sqlalchemy"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    dbname = argv[3]
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, dbname)
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    i = session.query(State).filter_by(id=2).first()
+    i.name = "New Mexico"
+    session.commit()
