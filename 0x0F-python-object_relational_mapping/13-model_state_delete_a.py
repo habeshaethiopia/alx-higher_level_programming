cd898e5018200e2c802609da81408957b3192cae
Aadane <adanemoges6@gmail.com>
2023-09-18 05:56:18 +0300

    task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""using sqlalchemy"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    dbname = argv[3]
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, dbname)
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for i in session.query(State).filter(State.
+                                         name.like("%a%")).order_by(State.id):
+        session.delete(i)
+    session.commit()
