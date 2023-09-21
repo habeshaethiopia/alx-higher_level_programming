cc378b11018fe47b4ec1876ddbf66cae29c72620
Adane <adanemoges6@gmail.com>
2023-09-21 19:41:39 +0300

    advance 3

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""the advanced task start module"""
+from sqlalchemy import create_engine, insert
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from relationship_state import State, Base
+from relationship_city import City
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    dbname = argv[3]
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, dbname)
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    conn = Session()
+    result = conn.query(State).all()
+    for s in result:
+        for i in s.cities:
+            if s.id == i.state_id:
+                  print("{}: {} -> {}".format(i.id, i.name, s.name))
