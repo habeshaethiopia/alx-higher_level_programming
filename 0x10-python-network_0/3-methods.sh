b8fc3534718f9714ecda7877e4b6770a4eccca4e
Adane <adanemoges6@gmail.com>
2023-10-01 17:08:17 +0300

    displaying all allow methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Displays all HTTP methods the server will accept.
-curl -sI -X OPTIONS "$1" | grep Allow | cut -d' ' -f1 --complement
+curl -sI "$1" | grep 'Allow' | cut -d' ' -f1 --complement
