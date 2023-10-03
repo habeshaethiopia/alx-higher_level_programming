011a6f1b608ba135c8601e510a92cad7e7503a95
Adane <adanemoges6@gmail.com>
2023-10-03 05:10:53 +0300

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@
  of the X-Request-Id variable found in the header of the response."""
 from sys import argv
 from urllib import request
-
-res = request.Request(argv[1])
-with request.urlopen(res) as response:
-    print(response.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    res = request.Request(argv[1])
+    with request.urlopen(res) as response:
+        print(response.headers.get("X-Request-Id"))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-"""sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
+"""sends a POST request to the passed URL 
+with the email as a parameter, and displays 
+the body of the response (decoded in utf-8)"""
+
