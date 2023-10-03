fff613453358b6e9f27de8d3cf24b80a6b462834
Adane <adanemoges6@gmail.com>
2023-10-03 04:50:04 +0300

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request
+
+res = request.Request("https://alx-intranet.hbtn.io/status")
+with request.urlopen(res) as response:
+    page = response.read()
+print("Body response:")
+print("\t- type:{}".format(type(page)))
+print("\t- content:{}".format(page))
+print("\t- utf8 content:{}".format(page.decode("utf-8")))
