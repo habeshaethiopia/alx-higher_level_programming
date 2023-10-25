5dd58e485de06ced38c05e9af61b10b4cdbde980
Adane <adanemoges6@gmail.com>
2023-10-25 22:40:15 +0300

    last 4

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,14 +8,14 @@ const url = `${argv[2]}`;
 const options = { json: true };
 request(url, options, (error, res, body) => {
   if (error) {
-    return console.log(error);
-  }
-
-  if (!error && res.statusCode === 200) {
-    fs.writeFile(argv[3], body, 'utf8', err => {
-      if (err) {
-        console.log(err);
-      }
-    });
+    console.log(error);
+  } else {
+    if (!error && res.statusCode === 200) {
+      fs.writeFile(argv[3], body, 'utf8', err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
   }
 });
