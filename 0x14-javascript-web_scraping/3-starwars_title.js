0dae838a9e428058b6b501420ac2c5130200ef8e
Adane <adanemoges6@gmail.com>
2023-10-25 20:59:43 +0300

    fix semistandards removing unused variable

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,6 @@ const request = require('request');
 const { argv } = require('process');
 
 const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
-let result;
 const options = { json: true };
 request(url, options, (error, res, body) => {
   if (error) {
