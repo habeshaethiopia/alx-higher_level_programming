57141874e543a46ff4252641de54b76f18d8506a
Adane <adanemoges6@gmail.com>
2023-10-25 15:07:21 +0300

    standard js code

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // read the file in js
-const fs = require("fs");
-const { argv } = require("process");
-fs.readFile(argv[2], "utf8", (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(data);
-});
+const fs = require('fs')
+const { argv } = require('process')
+fs.readFile(argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log(data)
+})
