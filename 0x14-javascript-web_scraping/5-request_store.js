a9365eac6a9e1be4ffb40fce4703b1782857a6ae
Adane <adanemoges6@gmail.com>
2023-10-25 22:24:56 +0300

    store response result in file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+// stotre the response in file
+const fs = require('fs');
+const { argv } = require('process');
+const request = require('request');
+const url = `${argv[2]}`;
+
+const options = { json: true };
+request(url, options, (error, res, body) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  if (!error && res.statusCode === 200) {
+    fs.writeFile(argv[3], body, err => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
