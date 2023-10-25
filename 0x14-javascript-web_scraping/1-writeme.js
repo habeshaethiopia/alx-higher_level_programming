3147bf11fafb3c0db5596f66c0e88cb318a02955
Adane <adanemoges6@gmail.com>
2023-10-25 16:39:35 +0300

    adding semicolon

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,4 +2,8 @@
 // write the file in js
 const fs = require('fs');
 const { argv } = require('process');
-fs.writeFile(argv[2], argv[3]);
+fs.writeFile(argv[2], argv[3], err => {
+  if (err) {
+    console.log(err);
+  }
+});
