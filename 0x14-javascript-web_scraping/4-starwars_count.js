a7356e610a64121e0eaefb815053ce8b7520c03a
Adane <adanemoges6@gmail.com>
2023-10-25 21:10:07 +0300

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// printing the status code
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/people/18';
+const options = { json: true };
+request(url, options, (error, res, body) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  if (!error && res.statusCode === 200) {
+    console.log(body.films.length);
+  }
+});
