98a9ebcb4caafff8bec3e8b4c03fc27f0bbcc462
Adane <adanemoges6@gmail.com>
2023-10-25 14:54:31 +0300

    including some readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,8 +3,8 @@
 const fs = require("fs");
 const { argv } = require("process");
 fs.readFile(argv[2], "utf8", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data);
+    if (err) {
+        console.log(err);
+    }
+    console.log(data);
 });
