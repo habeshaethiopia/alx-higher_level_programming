e9bd671d33cd95d750fc475135590aeefdbb1dab
Adane <adanemoges6@gmail.com>
2023-10-25 23:50:35 +0300

    advance 2

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,22 @@
 #!/usr/bin/node
 // print complited
 const request = require('request');
-const { argv } = require('process');
-const url = `${argv[2]}`;
+
 const options = { json: true };
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
 request(url, options, (error, res, body) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(body.reduce((acc, cur) => {
-      if (cur.completed) {
-        if (acc[cur.userId]) {
-          acc[cur.userId]++;
+    (body.characters)
+      .map(x => request(x, options, (error, res, body) => {
+        if (error) {
+          console.log(error);
         } else {
-          acc[cur.userId] = 1;
+          console.log(body.name);
         }
-      }
-      return acc;
-    }, {}));
+      }));
   }
 }
 );
