9e188efe18b309afbc26e7af28a5cd68fb3b4513
Adane <adanemoges6@gmail.com>
2023-10-25 22:00:58 +0300

    last 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,15 +2,18 @@
 // printing the status code
 const request = require('request');
 const { argv } = require('process');
-const url = (`${argv[2]}`).replace('films', 'people/18');
+const url = `${argv[2]}`;
 
 const options = { json: true };
 request(url, options, (error, res, body) => {
   if (error) {
-    return console.log(error);
+    console.log(error);
+    return;
   }
 
-  if (!error && res.statusCode === 200) {
-    console.log(body.films.length);
-  }
+  const films = body.results;
+  console.log(films.reduce((c, e) => {
+    e.characters.forEach(el => el.includes('18') ? c++ : c);
+    return c;
+  }, 0));
 });
