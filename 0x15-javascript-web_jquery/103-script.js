50ed31d538adc7b2b7d833c65dd69e58e368d234
Adane <adanemoges6@gmail.com>
2023-11-02 06:38:37 +0300

    last advacd listenning to enter

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,15 @@
+  window.onload = function () {
+    function translate() {
+        $.get('https://hellosalut.stefanbohacek.dev/?mode=' + $('#language_code').val(), function (data) {
+            $('DIV#hello').text(data.hello);
+        });
+    }
+
+    $('#btn_translate').click(translate);
+
+    $('#language_code').keypress(function (event) {
+        if (event.which == 13) {
+            translate();
+        }
+    });
+};
