42aa8546b5c682ebfcfc5ad83683783fde5b849f
Adane <adanemoges6@gmail.com>
2023-11-02 06:03:13 +0300

    advance 2

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,12 +1,13 @@
 
-$("DIV#add_item").on.("click", function () {
-    $("UL.my_list").append("<li>Item</li>");
-});
-$("DIV#remove_item").on.("click", function () {
-    $("UL.my_list").children().last().remove();
-});
+window.onload = function () {
+  $('DIV#add_item').on('click', function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').on('click', function () {
+    $('UL.my_list').children().last().remove();
+  });
 
-
-$("DIV#clear_list").on.("click", function () {
-    $("UL.my_list").empty();
-});
+  $('DIV#clear_list').on('click', function () {
+    $('UL.my_list').empty();
+  });
+};
